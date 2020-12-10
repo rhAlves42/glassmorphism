74934818ae076cd16288b3a842a83baed8a9eee0
Ricardo Henrique <tio.rick.rh@gmail.com>
2020-12-09 23:37:16 -0300

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+// const tweetContainer = document.getElementById('tweet');
+// const api = 'https://sv443.net/jokeapi/v2/joke/Any?format=txt&type=single';
+
+// const http = new XMLHttpRequest();
+// http.open('POST', api);
+// http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+// http.onreadystatechange = () => {
+//   if (http.readyState === 4 && http.status === 200) {
+//     alert(http.responseText);
+//   }
+// };
+
+// $(document).onReady(() => {
+//   alert('carregado')
+// })
